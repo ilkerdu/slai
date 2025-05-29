e336000cf08436110461997899eb3e456940a706
ilkerdu <utkata31@gmail.com>
2025-05-29 20:48:38 +0300

    news

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // EKLENDİ
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()], // EKLENDİ
+    define: {
+      'process.env': {
+        API_KEY: JSON.stringify(env.GEMINI_API_KEY),
+        GEMINI_API_KEY: JSON.stringify(env.GEMINI_API_KEY)
       }
-    };
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.')
+      }
+    },
+    build: {
+      outDir: 'dist' //
+    }
+  };
 });
